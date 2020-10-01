acce4f5f8462a5140e431888d2a6e1d12f4ac041
Job <LordChunk@users.noreply.github.com>
2020-10-01 16:15:09 +0000

    Added Dutch locales and translation

---
+++ locales/nl.lua
@@ -0,0 +1,9 @@
+Locales['nl'] = {
+  ['valid_purchase'] = 'aankoop goedkeuren',
+  ['yes'] = 'ja',
+  ['no'] = 'nee',
+  ['not_enough_money'] = 'je hebt niet genoeg geld',
+  ['press_access'] = 'druk op ~INPUT_CONTEXT~ om naar de ~y~Kapper~s~ te gaan.',
+  ['barber_blip'] = 'kapper',
+  ['you_paid'] = 'je betaalde ~g~€%s~s~',
+}
